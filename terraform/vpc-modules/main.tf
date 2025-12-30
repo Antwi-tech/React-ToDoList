18eb1c998ed381b950f602c173370d3c3f4a63c0
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-12-30 17:00:10 +0000

    creation of vpc and it's requirments for an instance

---
+++ terraform/vpc-modules/main.tf
@@ -0,0 +1,51 @@
+locals {
+  # normalized workspace name
+  environment = terraform.workspace == "default" ? "dev" : terraform.workspace
+ 
+  # base app/project name
+  project     = "app"
+ 
+  # standard prefix pattern for all resources
+  prefix      = "${local.project}-${local.environment}"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    name = "${local.prefix}-vpc"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  region = "us-east-1"
+  vpc_id = aws_vpc.my_vpc.id
+  availability_zone = var.az
+  cidr_block = var.subnet_cidr_block
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "${local.prefix}-igw"
+  } 
+}
+
+resource "aws_route_table" "my_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "${local.prefix}-rt"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_rt.id
+}
